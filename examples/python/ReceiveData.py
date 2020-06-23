7dc3ad938dda63f15b696043ca56f27b617e91a6
Diep NguyenVan <diepnguyen@emotiv.com>
2020-06-23 17:26:17 +0700

    add example python

---
+++ examples/python/ReceiveData.py
@@ -0,0 +1,20 @@
+"""Example program to show how to read a multi-channel time series from LSL."""
+import time
+from pylsl import StreamInlet, resolve_stream
+
+# first resolve an EEG stream on the lab network
+print("looking for a stream...")
+streams = resolve_stream('type', 'Motion')
+print(streams)
+
+# create a new inlet to read from the stream
+inlet = StreamInlet(streams[0])
+
+while True:
+    # get a new sample (you can also omit the timestamp part if you're not
+    # interested in it)
+    sample, timestamp = inlet.pull_sample()
+    time.sleep(0.5)
+    print(timestamp, sample)
+
+
